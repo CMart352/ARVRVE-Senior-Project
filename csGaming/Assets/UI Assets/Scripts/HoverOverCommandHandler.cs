13d201c1930fe3b47cb335b36d5a5e5aee153e3b
mbarr203 <31226202+mbarr203@users.noreply.github.com>
2018-03-26 14:36:31 -0400

    Add a delete button upon hovering of command

---
+++ csGaming/Assets/UI Assets/Scripts/HoverOverCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class HoverOverCommandHandler : MonoBehaviour , IPointerEnterHandler, IPointerExitHandler{
+
+	public GameObject removeButton;
+
+	#region IPointerEnterHandler implementation
+
+	public void OnPointerEnter (PointerEventData eventData)
+	{
+		removeButton.SetActive (true);
+		Debug.Log ("You have hovered over a command");
+	}
+
+	#endregion
+
+	#region IPointerExitHandler implementation
+
+	public void OnPointerExit (PointerEventData eventData)
+	{
+		removeButton.SetActive (false);
+	}
+
+	#endregion
+
+
+
+
+	void OnMouseEnter()
+	{
+		Debug.Log ("You have hovered over a command");
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
