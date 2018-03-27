570eb47bfccc82df115d456a4853510dd2263779
mbarr203 <31226202+mbarr203@users.noreply.github.com>
2018-03-27 11:22:37 -0400

    Implementation of delete button

---
+++ csGaming/Assets/UI Assets/Scripts/removeCommand.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class removeCommand : MonoBehaviour {
+
+	public Button removeSign;
+	public GameObject commandPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+		Button deteleButton = removeSign.GetComponent<Button>();
+		removeSign.onClick.AddListener(Remove);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Remove()
+	{
+		Destroy (commandPrefab);
+	}
+}
