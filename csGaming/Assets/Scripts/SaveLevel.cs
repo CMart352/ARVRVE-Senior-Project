825cac890b870546697fe2d30ac15a87bcbc81c7
Fidel Hernandez <fhern103@fiu.edu>
2018-04-03 15:32:42 -0400

    Save current level
    Save current level when player quits the game

---
+++ csGaming/Assets/Scripts/SaveLevel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SaveLevel : MonoBehaviour {
+
+	public void saveLevel() {
+		string level = SceneManager.GetActiveScene().name;
+		Debug.Log (level);
+
+		new GameSparks.Api.Requests.LogEventRequest ()
+			.SetEventKey ("SAVE_LEVEL")
+			.SetEventAttribute ("PATH", level)
+			.Send ((response) => {
+				if (!response.HasErrors) {
+					Debug.Log("Level saved successfully");
+				}
+				else {
+					Debug.Log("Error " + response.Errors.JSON.ToString());
+				}
+		});
+
+	}
+}
