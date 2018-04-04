fb3b7ca359c12c695ac07f3257e29e97e148d926
Arelys Alvarez <aalva456@fiu.edu>
2018-04-03 20:50:59 -0400

    finish a waypoint tool
    finished creating a waypoint tool

---
+++ csGaming/Assets/WaypointsTool/WaypointsToolMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace WaypointsTool
+{
+    public class WaypointsToolMenu
+    {
+        //menu item for the waypoint tool
+        [MenuItem("WaypointsTool/Create/Waypoints")]
+        static void CreateWaypoints(){
+            GameObject wpo = new GameObject();
+            wpo.AddComponent<WaypointsScript>();
+            wpo.name = "Waypoints";
+
+
+            Selection.activeObject = wpo;
+            Undo.RegisterCreatedObjectUndo(wpo, "Created " + wpo.name);
+        }
+       
+    }
+}
